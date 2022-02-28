49dccd86688eab6675658a0be1c3795a22d0f734
Carmen Kaplan <viajero19976@innovaccion.mx>
2022-02-28 08:15:04 -0600

    URL update

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
 import web
 
 urls = [
-    '/','mvc.controllers.index.Index', 
+    '/','mvc.controllers.index.Index',
+    '/calendario/?','mvc.controllers.calendario.Calendario',
+    '/comunidad/?','mvc.controllers.comunidad.Comunidad',
+    '/conocenos/?','mvc.controllers.conocenos.Conocenos',
+    '/contaduria/?','mvc.controllers.contaduria.Contaduria',
+    '/criminalistica/?','mvc.controllers.criminalistica.Criminalistica',
+    '/desarrollo/?','mvc.controllers.desarrollo.Desarrollo',
+    '/digital/?','mvc.controllers.digital.Digital',
+    '/enfermeria/?','mvc.controllers.enfermeria.Enfermeria',
     '/index/?','mvc.controllers.index.Index',
+    '/industrial/?','mvc.controllers.industrial.Industrial',
+    '/mecatronica/?','mvc.controllers.mecatronica.Mecatronica',
+    '/nanotecnologia/?','mvc.controllers.nanotecnologia.Nanotecnologia',
+    '/negocios/?','mvc.controllers.negocios.Negocios',
+    '/ofertaeducativa/?','mvc.controllers.ofertaeducativa.Ofertaeducativa',
     '/profile/?','mvc.controllers.profile.Profile',
+    '/renovables/?','mvc.controllers.renovables.Renovables',
+    '/reproductiva/?','mvc.controllers.reproductiva.Reproductiva',
+    '/sistemas/?','mvc.controllers.sistemas.Sistemas',
+    '/terapia/?','mvc.controllers.terapia.Terapia',
+    '/transparencia/?','mvc.controllers.transparencia.Transparencia',
 
 ] # COLOCAMOS LA RUTA
 
